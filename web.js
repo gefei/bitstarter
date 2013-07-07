010bfb94c2ad0ab352a1f9c40ff111d3dd1d4bda
Gefei Zhang <g.zhang@celonis.de>
2013-07-07 20:55:06 +0200

    Showing message from index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var x = fs.readFileSync('index.html');
+    response.send(x.toString());
+    // response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
